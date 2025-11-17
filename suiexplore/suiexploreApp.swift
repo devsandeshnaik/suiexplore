14349451a4cf94eef1e85df6d16745ff3244f6c4
sandesh.naik <sandesh.mynt@perennailsys.com>
2025-11-17 14:10:36 +0530

    Initial Commit

---
+++ suiexplore/suiexploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiexploreApp.swift
+//  suiexplore
+//
+//  Created by Sandesh Naik on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct suiexploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
